96ec7f68b5d817acf47657de49869dc8ab004c61
Brian Kendzior <bkendzior@gmail.com>
2015-07-20 17:48:16 -0700

    edit install script

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 # create symlinks
 ln -sf $HOME/.bkendzior/.bashrc $HOME/.bash_profile
-
 ln -sf $HOME/.bkendzior/.bashrc $HOME/.bashrc
-
 ln -sf $HOME/.bkendzior/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/.bkendzior/.vimrc $HOME/.vimrc
 ln -sf $HOME/.bkendzior/.tmux.conf $HOME/.tmux.conf
