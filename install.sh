2d37f3335d44d6de8e2003ddfd7635e41d9fd689
Brian Kendzior <bkendzior@Brians-Mac-mini.local>
2015-07-20 17:38:30 -0700

    Adding gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # create symlinks
-ln -s $HOME/.bkendzior/.bashrc $HOME/.bash_profile
-ln -s $HOME/.bkendzior/.bashrc $HOME/.bashrc
-ln -s $HOME/.bkendzior/.vimrc $HOME/.vimrc
-ln -s $HOME/.bkendzior/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.bkendzior/.bashrc $HOME/.bash_profile
+
+ln -sf $HOME/.bkendzior/.bashrc $HOME/.bashrc
+
+ln -sf $HOME/.bkendzior/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/.bkendzior/.vimrc $HOME/.vimrc
+ln -sf $HOME/.bkendzior/.tmux.conf $HOME/.tmux.conf
